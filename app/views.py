212b183f3c8b3033c8c12b5b0fcfcc10537205e2
Adam Hernandez <hernandezdavid88@utexas.edu>
2016-02-20 12:40:26 -0600

    Navbar changes

--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
 from flask import render_template, flash, redirect, url_for, session, request, g
 from app import app
 
+nav = 'home, projects, about'
+
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
